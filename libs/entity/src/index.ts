f74078f712a84444c9f55ef4034d8e31f020b71a
Tim Aerts <t.aerts@hotmail.com>
2022-12-07 01:35:41 +0100

    Fix broken api perhaps?

--- libs/entity/src/index.ts
+++ libs/entity/src/index.ts
@@ -3,4 +3,3 @@ export * from './lib/entities/user/user.interface';
 export * from './lib/data/auth.interface';
 export * from './lib/data/id.type';
 export * from './lib/data/api-response.interface';
-export * from './lib/entities/user/user.module';
